cb684ae5b61a553a01f401a109d4d4f40e8c2480
Mohammed suhail <119152757+ImmortalXnetwork@users.noreply.github.com>
2023-05-31 10:33:33 +0530

    Add files via upload

---
+++ anibot/__main__.py
@@ -0,0 +1,24 @@
+import asyncio
+from pyrogram import idle
+from . import anibot, has_user, session
+from .utils.db import _close_db
+from uvloop import install
+
+user = None
+if has_user:
+    from . import user
+
+async def main():
+    install()
+    await anibot.start()
+    if user is not None:
+        await user.start()
+    await idle()
+    await anibot.stop()
+    if user is not None:
+        await user.stop()
+    _close_db()
+    await session.close()
+
+
+asyncio.get_event_loop().run_until_complete(main())
